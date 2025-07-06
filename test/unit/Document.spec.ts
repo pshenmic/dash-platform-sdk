57bf534cdc30e66cee58160af3a0bc5c5093eecc
pshenmic <pshenmic@gmail.com>
2025-07-06 20:12:44 +0700

    Commit forgotten changes

--- test/unit/Document.spec.ts
+++ test/unit/Document.spec.ts
@@ -10,7 +10,7 @@ describe('Document', () => {
 
   test('should be able to create document', async () => {
     const dataContract = '6QMfQTdKpC3Y9uWBcTwXeY3KdzRLDqASUsDnQ4MEc9XC'
-    const identity = 'B7kcE1juMBWEWkuYRJhVdAE2e6RaevrGxRsa1DrLCpQH'
+    const identity = 'QMfCRPcjXoTnZa9sA9JR2KWgGxZXMRJ4akgS3Uia1Qv'
     const revision = BigInt(1)
     const documentType = 'pool'
     const data = {
