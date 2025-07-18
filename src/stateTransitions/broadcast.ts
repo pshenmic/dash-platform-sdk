a181f506913e35d4e4e922afa24c987fa9d73a4e
pshenmic <pshenmic@gmail.com>
2025-07-18 16:45:39 +0700

    Add check before broadcast

--- src/stateTransitions/broadcast.ts
+++ src/stateTransitions/broadcast.ts
@@ -3,6 +3,10 @@ import { StateTransitionWASM } from 'pshenmic-dpp'
 import GRPCConnectionPool from '../grpcConnectionPool'
 
 export default async function broadcast (grpcPool: GRPCConnectionPool, stateTransition: StateTransitionWASM): Promise<void> {
+  if (stateTransition.signature.length === 0) {
+    throw new Error('State Transition is not signed')
+  }
+
   const broadcastStateTransitionRequest = BroadcastStateTransitionRequest.fromPartial({
     stateTransition: stateTransition.bytes()
   })
