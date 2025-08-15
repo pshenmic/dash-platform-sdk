d8e95a01ef240a1dfa541afb41c28edede29efb5
pshenmic <pshenmic@gmail.com>
2025-08-15 15:51:35 +0700

    Add check for response status

--- src/utils/getEvonodeList.ts
+++ src/utils/getEvonodeList.ts
@@ -5,6 +5,10 @@ export default async function getDAPINodeList (network: 'testnet' | 'mainnet'): 
 
   const resp = await fetch(url)
 
+  if (resp.status !== 200) {
+    throw new Error('Failed to query Platform Explorer for active validators')
+  }
+
   const { resultSet } = await resp.json()
 
   return resultSet
