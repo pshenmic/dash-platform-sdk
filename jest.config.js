358d1877f815473788943be8844f81c641ce3d1f
pshenmic <pshenmic@gmail.com>
2025-08-15 15:52:54 +0700

    Lint fix

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
   transformIgnorePatterns: [
     '/node_modules/(?!pshenmic-dpp|wasm-drive-verify)'
   ],
