ea1012ddef4f8decf1a20d2bf57173766f2cb875
pshenmic <pshenmic@gmail.com>
2025-08-17 07:40:17 +0700

    Add transfer transition creation tests

--- test/unit/Tokens.spec.ts
+++ test/unit/Tokens.spec.ts
@@ -1,4 +1,5 @@
 import { DashPlatformSDK } from '../../src/types'
+import {StateTransitionWASM, TokenBaseTransitionWASM} from "pshenmic-dpp";
 
 let sdk: DashPlatformSDK
 
@@ -18,7 +19,7 @@ describe('Tokens', () => {
   test('should be able to get token contract info', async () => {
     const tokenContractInfo = await sdk.tokens.getTokenContractInfo('9YxdbQUjJmQsmVPen95HjAU3Esj7tVkWSY2EQWT84ZQP')
 
-    expect(tokenContractInfo.contractId.base58()).toEqual('Y189uedQG3CJCuu83P3DqnG7ngQaRKz69x3gY8uDzQe')
+    expect(tokenContractInfo.dataContractId.base58()).toEqual('Y189uedQG3CJCuu83P3DqnG7ngQaRKz69x3gY8uDzQe')
     expect(tokenContractInfo.tokenContractPosition).toEqual(0)
   })
 
@@ -36,5 +37,21 @@ describe('Tokens', () => {
     expect(tokensIdentityBalance.length).toEqual(1)
     expect(tokensIdentityBalance[0].identityId).toBeTruthy()
     expect(tokensIdentityBalance[0].balance).toBeTruthy()
+  })
+
+  test('should be able to create base token transition', async () => {
+    const tokenBaseTransition = await sdk.tokens.createBaseTransition('A36eJF2kyYXwxCtJGsgbR3CTAscUFaNxZN19UqUfM1kw', '34vkjdeUTP2z798SiXqoB6EAuobh51kXYURqVa9xkujf')
+
+    expect(tokenBaseTransition).toBeInstanceOf(TokenBaseTransitionWASM)
+  })
+
+  test('should be able to create transfer transition', async () => {
+    const owner = '34vkjdeUTP2z798SiXqoB6EAuobh51kXYURqVa9xkujf'
+    const recipient = 'FQ4waDowFQXD4tJPKQM1114VSr5f8s3qAc5bT8FJkT49'
+    const tokenBaseTransition = await sdk.tokens.createBaseTransition('A36eJF2kyYXwxCtJGsgbR3CTAscUFaNxZN19UqUfM1kw', 'FQ4waDowFQXD4tJPKQM1114VSr5f8s3qAc5bT8FJkT49')
+
+    const stateTransition = sdk.tokens.createStateTransition(tokenBaseTransition, owner, 'transfer', { identityId: recipient, amount: BigInt(1000)})
+
+    expect(stateTransition).toBeInstanceOf(StateTransitionWASM)
   })
 })
