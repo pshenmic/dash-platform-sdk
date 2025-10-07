7b1d7acedc9268d542880523c809504d7eac9997
pshenmic <pshenmic@gmail.com>
2025-10-07 15:45:12 +0700

    Add condition for pnpm

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 export default {
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   transformIgnorePatterns: [
-    '/node_modules/(?!pshenmic-dpp|@protobuf-ts/grpcweb-transport|@scure*|@noble*|cbor-x)'
+    '/node_modules/(?!(?:.pnpm/))(?!pshenmic-dpp|@protobuf-ts/grpcweb-transport|@scure*|@noble*|cbor-x)'
   ],
   // At this moment tests runs in parallel processes with timeout 40 sec
   // If you need single run - uncomment line bellow
