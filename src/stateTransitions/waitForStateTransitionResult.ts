2f2762f6b8995a919eeace4972c4273cc48effa6
pshenmic <pshenmic@gmail.com>
2025-11-28 02:04:40 +0700

    Migrate waitForStateTransitionResult to DAPI

--- src/stateTransitions/waitForStateTransitionResult.ts
+++ src/stateTransitions/waitForStateTransitionResult.ts
@@ -1,13 +1,38 @@
 import { StateTransitionWASM } from 'pshenmic-dpp'
 import GRPCConnectionPool from '../grpcConnectionPool.js'
+import hexToBytes from "../utils/hexToBytes.js";
+import {WaitForStateTransitionResultRequest} from "../../proto/generated/platform.js";
 
 export default async function waitForStateTransitionResult (grpcPool: GRPCConnectionPool, stateTransition: StateTransitionWASM): Promise<void> {
-  const url = `https://${grpcPool.network === 'mainnet' ? '' : 'testnet.'}platform-explorer.pshenmic.dev/waitForStateTransitionResult/${stateTransition.hash(false)}`
+  const txHash = stateTransition.hash(false)
 
-  const resp = await fetch(url)
+  const waitForStateTransitionResultRequest = WaitForStateTransitionResultRequest.create({
+    version: {
+      oneofKind: "v0",
+      v0: {
+        stateTransitionHash: hexToBytes(txHash),
+        prove: false
+      }
+    }
+  })
 
-  if (resp.status !== 200) {
-    console.log(await resp.json())
-    throw new Error('Internal server error while waiting for state transition result')
+  const {response} = await grpcPool.getClient().waitForStateTransitionResult(waitForStateTransitionResultRequest)
+
+  const { version } = response
+
+  if (version.oneofKind !== 'v0') {
+    throw new Error('Unexpected oneOf type returned from DAPI (must be v0)')
+  }
+
+  const { v0 } = version
+
+  if (v0.result.oneofKind === undefined) {
+    // all ok
+    return
+  } else if (v0.result.oneofKind === 'error') {
+    const {code, data, message} = v0.result.error
+    throw new Error(`State transition ${txHash} failed to execute with code ${code}, message: ${message}`)
+  } else {
+    throw new Error('Unexpected oneOfKind returned for waitForStateTransitionResult')
   }
 }
