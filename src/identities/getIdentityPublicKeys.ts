da1b0bdfebf341ea86f773472e287b97344ffb5a
pshenmic <pshenmic@gmail.com>
2025-12-04 21:42:51 +0700

    Fix import

--- src/identities/getIdentityPublicKeys.ts
+++ src/identities/getIdentityPublicKeys.ts
@@ -5,7 +5,7 @@ import GRPCConnectionPool from '../grpcConnectionPool.js'
 import { getQuorumPublicKey } from '../utils/getQuorumPublicKey.js'
 import bytesToHex from '../utils/bytesToHex.js'
 import verifyTenderdashProof from '../utils/verifyTenderdashProof.js'
-import {LATEST_PLATFORM_VERSION} from "../constants";
+import {LATEST_PLATFORM_VERSION} from "../constants.js";
 
 export default async function getIdentityPublicKeys (grpcPool: GRPCConnectionPool, identifier: IdentifierLike, keyIds?: number[]): Promise<IdentityPublicKeyWASM[]> {
   const id = new IdentifierWASM(identifier)
